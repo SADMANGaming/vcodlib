6e67e97aea03da4d1826d3cb4ea2d291db50584b
SADMANGaming <74666591+SADMANGaming@users.noreply.github.com>
2024-09-18 16:01:48 +0600

    Delete src/gsc_bgame.hpp

--- src/gsc_bgame.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef _GSC_BGAME_HPP_
-#define _GSC_BGAME_HPP_
-
-#include "gsc.hpp"
-
-void G_AddPredictableEvent( gentity_t *ent, int event, int eventParm );
-void BG_AddPredictableEventToPlayerstate( int newEvent, int eventParm, playerState_t *ps );
-
-#endif
